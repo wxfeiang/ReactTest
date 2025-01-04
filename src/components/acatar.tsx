822a95b27478baca1c763135932dd206307f0105
wxfeiang <wxfeiang@qq.com>
2025-01-04 15:03:56 +0800

    feat: router

--- src/components/acatar.tsx
+++ src/components/acatar.tsx
@@ -4,59 +4,65 @@ import React from "react"
 import { Table } from "antd"
 import { alovaInstance } from "../utils/http"
 interface AvatarProps {
-  size: number
+    size: number
 }
 interface User {
-  id: number
-  name: string
-  email: string
+    id: number
+    name: string
+    email: string
 }
 const columns = [
-  {
-    title: "姓名",
-    dataIndex: "name",
-    key: "name"
-  },
-  {
-    title: "年龄",
-    dataIndex: "id",
-    key: "id"
-  },
-  {
-    title: "住址",
-    dataIndex: "email",
-    key: "email"
-  }
+    {
+        title: "姓名",
+        dataIndex: "name",
+        key: "name"
+    },
+    {
+        title: "年龄",
+        dataIndex: "id",
+        key: "id"
+    },
+    {
+        title: "住址",
+        dataIndex: "email",
+        key: "email"
+    },
+    {
+        title: 'Action',
+        dataIndex: '',
+        key: 'Action',
+        render: () => <a onClick={(row) => { console.log(row) }}>Delete</a>,
+    },
 ]
 
 const Avatar: React.FC<AvatarProps> = (props) => {
-  console.log("🥡[size]:", props)
+    console.log("🥡[size]:", props)
 
-  // 使用alova实例创建method并传给useRequest即可发送请求
-  const { loading, data, error } = useRequest(
-    alovaInstance.Get<User[]>("/users"),
-    {
-      initialData: [], // 设置data状态的初始数据
-      immediate: true // 是否立即发送请求，默认为true
+    // 使用alova实例创建method并传给useRequest即可发送请求
+    const { loading, data, error } = useRequest(
+        alovaInstance.Get<User[]>("/users"),
+        {
+            initialData: [], // 设置data状态的初始数据
+            immediate: true // 是否立即发送请求，默认为true
+        }
+    )
+    if (error) {
+        return <div>error</div>
     }
-  )
-  if (error) {
-    return <div>error</div>
-  }
-  function itemclick(item: User) {
-    console.log(item)
-  }
+    function itemclick(item: User) {
+        console.log(item)
+    }
 
-  return (
-    <>
-      <Table
-        dataSource={data}
-        columns={columns}
-        rowKey={(record) => record.id}
-        loading={loading}
-      />
-    </>
-  )
+    return (
+        <>
+            <Table
+                dataSource={data}
+                columns={columns}
+                rowKey={(record) => record.id}
+                loading={loading}
+            />
+        </>
+    )
 }
 
 export default Avatar
